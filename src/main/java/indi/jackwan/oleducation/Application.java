5963db910355bdfc3814ae87c2ca76aa8dbce591
WanNJ <omyjackwan@gmail.com>
2018-03-07 20:19:32 +0800

    Registeration v2.0

--- src/main/java/indi/jackwan/oleducation/Application.java
+++ src/main/java/indi/jackwan/oleducation/Application.java
@@ -1,5 +1,5 @@
 // The recommended practice is to put every file under a root package.
-package oleducation;
+package indi.jackwan.oleducation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
