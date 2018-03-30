a15a67742dcf991e07b8c9949c5964ace190c3c1
WanNJ <omyjackwan@gmail.com>
2018-03-30 18:07:49 +0800

    Add score view

--- src/main/java/indi/jackwan/oleducation/controllers/user/UserScoreController.java
+++ src/main/java/indi/jackwan/oleducation/controllers/user/UserScoreController.java
@@ -1,11 +1,11 @@
 package indi.jackwan.oleducation.controllers.user;
 
+import indi.jackwan.oleducation.models.User;
 import indi.jackwan.oleducation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -17,7 +17,15 @@ public class UserScoreController {
 
     @RequestMapping(value = "/user/score", method = RequestMethod.GET)
     public String getScorePage(Model model, HttpSession session, RedirectAttributes redir) {
+        User old = (User) session.getAttribute("user");
+        session.setAttribute("user", userService.findByEmail(old.getEmail()));
         model.addAttribute("user", session.getAttribute("user"));
         return "user/score";
+    }
+
+    @RequestMapping(value = "/user/score", method = RequestMethod.POST)
+    public String transferScore(Model model, HttpSession session, RedirectAttributes redir,
+                                @RequestParam("transferScoreAmount") double amount, @RequestParam("bankAccount") String bankAccount) {
+        return "redirect:/user/score";
     }
 }
