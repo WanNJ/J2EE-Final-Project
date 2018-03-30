ede60b2bbff497a73082eee05d1049b39358288b
WanNJ <omyjackwan@gmail.com>
2018-03-30 19:12:14 +0800

    Finish feature: user can transfer scores to cash

--- src/main/java/indi/jackwan/oleducation/controllers/user/UserScoreController.java
+++ src/main/java/indi/jackwan/oleducation/controllers/user/UserScoreController.java
@@ -2,6 +2,7 @@ package indi.jackwan.oleducation.controllers.user;
 
 import indi.jackwan.oleducation.models.User;
 import indi.jackwan.oleducation.service.UserService;
+import indi.jackwan.oleducation.service.VipService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpSession;
 public class UserScoreController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private VipService vipService;
 
     @RequestMapping(value = "/user/score", method = RequestMethod.GET)
     public String getScorePage(Model model, HttpSession session, RedirectAttributes redir) {
@@ -26,6 +29,18 @@ public class UserScoreController {
     @RequestMapping(value = "/user/score", method = RequestMethod.POST)
     public String transferScore(Model model, HttpSession session, RedirectAttributes redir,
                                 @RequestParam("transferScoreAmount") double amount, @RequestParam("bankAccount") String bankAccount) {
+        User old = (User) session.getAttribute("user");
+        User newUser = userService.findByEmail(old.getEmail());
+
+        if (vipService.transferScore(newUser, bankAccount, amount)) {
+            redir.addFlashAttribute("successMessage", "Transfer succeeded!");
+        } else {
+            redir.addFlashAttribute("errorMessage", "Oops, something went wrong!");
+        }
+
+        session.setAttribute("user", userService.findByEmail(newUser.getEmail()));
+        model.addAttribute("user", session.getAttribute("user"));
+
         return "redirect:/user/score";
     }
 }
--- src/main/java/indi/jackwan/oleducation/service/VipService.java
+++ src/main/java/indi/jackwan/oleducation/service/VipService.java
@@ -1,7 +1,10 @@
 package indi.jackwan.oleducation.service;
 
+import indi.jackwan.oleducation.models.BankAccount;
 import indi.jackwan.oleducation.models.User;
+import indi.jackwan.oleducation.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,23 +12,46 @@ import org.springframework.stereotype.Service;
  */
 @Service("vipService")
 public class VipService {
+    @Value("${app.bankaccount.root}")
+    private String rootBankAccount;
+
     @Autowired
     private UserService userService;
+    @Autowired
+    private PaymentService paymentService;
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
 
     public double getDiscount(int userId) {
         User user = userService.findById(userId);
         double expenditure = user.getExpenditure();
         if (expenditure < 100) {
             return 1;
-        }
-        else if (100 <= expenditure && expenditure < 300) {
+        } else if (100 <= expenditure && expenditure < 300) {
             return 0.9;
-        }
-        else if (300 <= expenditure && expenditure < 500) {
+        } else if (300 <= expenditure && expenditure < 500) {
             return 0.8;
-        }
-        else {
+        } else {
             return 0.6;
+        }
+    }
+
+    public boolean transferScore(User user, String bankAccount, double amount) {
+        if (user.getScore() < amount) {
+            return false;
+        }
+
+        BankAccount account = bankAccountRepository.findByAccountAddress(bankAccount);
+
+        if (account == null)
+            return false;
+        else {
+            if (paymentService.transfer(rootBankAccount, bankAccount, amount / 20)) {
+                user.setScore(user.getScore() - amount);
+                userService.save(user);
+                return true;
+            }
+            return false;
         }
     }
 }
